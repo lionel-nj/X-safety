038fbde9034754be728538041e547b159a43cb6f
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-PO-62-62.nat.polymtl.ca>
2018-08-31 15:24:18 -0400

    v :int -> vecteurs

--- cars.py
+++ cars.py
@@ -54,24 +54,23 @@ l=random.normalvariate(6.5,0.3)
 posV=[moving.Point(2000,0)]
 L=[]
 L.append(l)
-
-v0=random.normalvariate(30,3.2)
-speed=[0]
+v0=moving.Point.__mul__(moving.Point(1,0),random.normalvariate(30,3.2))
+speed=[moving.Point(0,0)]
 
 for t in range(1,t_marche1):
-    speed.append(v0)
+    speed.append(moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0)))
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(0,t_stop):
-    speed.append(0)
+    speed.append(moving.Point(0,0))
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(0,t_marche2):
-    speed.append(v0)
+    speed.append(moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0)))
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(1,t_simul):
-    posV.append(position(posV[t-1].y,speed[t],1))
+    posV.append(position(posV[t-1].y,moving.Point.norm2(speed[t]),1))
 
 voie_verticale[0].timeInterval=[0,300]
 voie_verticale[0].positions=posV
@@ -86,49 +85,34 @@ voie_verticale[0].userType=1
 
 
 for k in range(1,number_of_cars):
-    v0=random.normalvariate(30,3.2)
+    v0=moving.Point.__mul__(moving.Point(1,0),random.normalvariate(30,3.2))
     l=random.uniform(6,8)
     L.append(l)
 
     voie_verticale[k]=moving.MovingObject()
     voie_verticale[k].timeInterval=moving.TimeInterval(intervals[k][0],300+intervals[k][0])
     voie_verticale[k].positions=[moving.Point(2000,0)]
-    voie_verticale[k].velocities=[0]
+    voie_verticale[k].velocities=[moving.Point(0,0)]
     voie_verticale[k].geometry=shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
     voie_verticale[k].userType=1
 
     for t in range(1,t_simul):
         p=moving.MovingObject.getPositions(voie_verticale[k])[t-1].y
-        v=moving.MovingObject.getVelocities(voie_verticale[k-1])[t]
-        # DV=moving.MovingObject.getVelocities(voie_verticale[k])[t-1]-moving.MovingObject.getVelocities(voie_verticale[k-1])[t-1]
-        velocite=v0
-        new_position=position(p,velocite,t)
-        # voie_verticale[k].positions.append(new_position)
+        v=moving.Point.norm2(moving.MovingObject.getVelocities(voie_verticale[k-1])[t])
+        velocite=moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0))
+        new_position=position(p,moving.Point.norm2(velocite),t)
 
         s=gap(moving.MovingObject.getPositions(voie_verticale[k-1])[t].y,new_position.y,L[k-1])
         smin=7
 
-        # if velocite>v0:
-        #     velocite=v0
-
         if s<smin:
-            velocite=(v*t-L[k-1]-smin)/(t)
+            velocite=moving.Point.__mul__(moving.Point(1,0),(v*t-L[k-1]-smin)/t)
 
-        if velocite<0:
-            velocite=0
-
+        if moving.Point.norm2(velocite)<0:
+            velocite=moving.Point(0,0)
 
         voie_verticale[k].velocities.append(velocite)
-        voie_verticale[k].positions.append(position(p,velocite,1))
-
-
-
-#         # if new_position.y<moving.MovingObject.getPositions(voie_verticale[k])[t-1].y:
-#         #     new_position=moving.MovingObject.getPositions(voie_verticale[k])[t-1]
-#         #
-#         # if velocite==0:
-#         #     voie_verticale[k].positions.append(moving.MovingObject.getPositions(voie_verticale[k])[t-1])
-#         # else:
+        voie_verticale[k].positions.append(position(p,moving.Point.norm2(velocite),1))
 
 create_yaml('voie_verticale.yml',voie_verticale)
 
@@ -137,14 +121,18 @@ p=[]
 v=[]
 plt.figure()
 
-for k in range (0,5):
+for k in range (0,number_of_cars):
     p.append([])
+    v.append([])
     t.append(intervals[k])
-    v.append(voie_verticale[k].velocities)
+
     for time in range(0,t_simul):
         p[k].append(voie_verticale[k].positions[time].y)
+        v[k].append(moving.Point.norm2(voie_verticale[k].velocities[time]))
 
-    plt.plot(t[k],v[k])
+
+
+    plt.plot(t[k],p[k])
     # plt.plot(t[k],p[k])
 # plt.plot(t,p)
 #
