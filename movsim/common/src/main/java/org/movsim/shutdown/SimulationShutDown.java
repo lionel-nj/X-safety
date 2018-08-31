238a8b8322213c42de6ee31e9f5224348711d0e3
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-PO-62-62.nat.polymtl.ca>
2018-08-31 11:26:37 -0400

    suppression fichiers inutiles

--- movsim/common/src/main/java/org/movsim/shutdown/SimulationShutDown.java
+++
@@ -1,9 +0,0 @@
-package org.movsim.shutdown;
-
-public interface SimulationShutDown {
-
-    /**
-     * Callback to allow the application to close all open resources in an ordered way before the program exits.
-     */
-    public void onShutDown();
-}
--- movsim/xsd/src/main/java/org/movsim/simulator/vehicles/longitudinalmodel/acceleration/parameter/IModelParameterNewell.java
+++
@@ -1,9 +0,0 @@
-package org.movsim.simulator.vehicles.longitudinalmodel.acceleration.parameter;
-
-/**
- *
- */
-// TODO documentation like in IModelParameterIDM
-public interface IModelParameterNewell extends IModelParameter {
-
-}
