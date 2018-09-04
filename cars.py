618b9f971ecad2c6cef70eee7967224d219e22c6
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-LA-30-75.nat.polymtl.ca>
2018-09-04 16:07:53 -0400

    mul

--- cars.py
+++ cars.py
@@ -54,11 +54,11 @@ l=random.normalvariate(6.5,0.3)
 posV=[moving.Point(2000,0)]
 L=[]
 L.append(l)
-v0=moving.Point.__mul__(moving.Point(1,0),random.normalvariate(30,3.2))
+v0=moving.Point(0,1).__mul__(random.normalvariate(30,3.2))
 speed=[moving.Point(0,0)]
 
 for t in range(1,t_marche1):
-    speed.append(moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0)))
+    speed.append(moving.Point(0,1).__mul__(moving.Point.norm2(v0)))
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(0,t_stop):
@@ -66,7 +66,7 @@ for t in range(0,t_stop):
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(0,t_marche2):
-    speed.append(moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0)))
+    speed.append(moving.Point(1,0).__mul__(moving.Point.norm2(v0)))
     # posV.append(position(posV[t-1].y,v0,1))
 
 for t in range(1,t_simul):
@@ -85,7 +85,7 @@ voie_verticale[0].userType=1
 
 
 for k in range(1,number_of_cars):
-    v0=moving.Point.__mul__(moving.Point(1,0),random.normalvariate(30,3.2))
+    v0=moving.Point(0,1).__mul__(random.normalvariate(30,3.2))
     l=random.uniform(6,8)
     L.append(l)
 
@@ -99,16 +99,16 @@ for k in range(1,number_of_cars):
     for t in range(1,t_simul):
         p=moving.MovingObject.getPositions(voie_verticale[k])[t-1].y
         v=moving.Point.norm2(moving.MovingObject.getVelocities(voie_verticale[k-1])[t])
-        velocite=moving.Point.__mul__(moving.Point(1,0),moving.Point.norm2(v0))
+        velocite=moving.Point(0,1).__mul__(moving.Point.norm2(v0))
         new_position=position(p,moving.Point.norm2(velocite),t)
 
         s=gap(moving.MovingObject.getPositions(voie_verticale[k-1])[t].y,new_position.y,L[k-1])
         smin=7
 
         if s<smin:
-            velocite=moving.Point.__mul__(moving.Point(1,0),(v*t-L[k-1]-smin)/t)
+            velocite=moving.Point(0,1).__mul__((v*t-L[k-1]-smin)/t)
 
-        if moving.Point.norm2(velocite)<0:
+        if velocite.y<0:
             velocite=moving.Point(0,0)
 
         voie_verticale[k].velocities.append(velocite)
--- cars_h.py
+++ cars_h.py
@@ -20,8 +20,8 @@ def gap(x_leader,x_following,L_leader):
 
 "fonction de maj des positions"
 
-def position(y,v,t):
-    return moving.Point(v*t+y,2000)
+def position(x,v,t):
+    return moving.Point(v*t+x,2000)
 
 "définition des instants de création des véhicules"
 
@@ -54,24 +54,24 @@ l=random.normalvariate(6.5,0.3)
 posH=[moving.Point(0,2000)]
 L=[]
 L.append(l)
+v0=moving.Point(1,0).__mul__(random.normalvariate(30,3.2))
+speed=[moving.Point(0,0)]
 
-v0=random.normalvariate(30,3.2)
-speed=[0]
+# for t in range(1,t_marche1):
+#     speed.append(moving.Point(0,0))
+    # poSh.append(position(poSh[t-1].y,v0,1))
 
-for t in range(1,t_marche1):
-    speed.append(v0)
-    # posH.append(position(posH[t-1].x,v0,1))
+for t in range(0,t_simul):
+    speed.append(moving.Point(1,0).__mul__(moving.Point.norm2(v0)))
 
-for t in range(0,t_stop):
-    speed.append(0)
-    # posH.append(position(posH[t-1].x,v0,1))
+    # poSh.append(position(poSh[t-1].y,v0,1))
 
-for t in range(0,t_marche2):
-    speed.append(v0)
-    # posH.append(position(posH[t-1].x,v0,1))
+# for t in range(0,t_marche2):
+#     speed.append(moving.Point(0,0))
+    # poSh.append(position(poSh[t-1].y,v0,1))
 
 for t in range(1,t_simul):
-    posH.append(position(posH[t-1].x,speed[t],1))
+    posH.append(position(posH[t-1].x,moving.Point.norm2(speed[t]),1))
 
 voie_horizontale[0].timeInterval=[0,300]
 voie_horizontale[0].positions=posH
@@ -86,52 +86,54 @@ voie_horizontale[0].userType=1
 
 
 for k in range(1,number_of_cars):
-    v0=random.normalvariate(30,3.2)
+    v0=moving.Point(1,0).__mul__(random.normalvariate(30,3.2))
     l=random.uniform(6,8)
     L.append(l)
 
     voie_horizontale[k]=moving.MovingObject()
     voie_horizontale[k].timeInterval=moving.TimeInterval(intervals[k][0],300+intervals[k][0])
     voie_horizontale[k].positions=[moving.Point(0,2000)]
-    voie_horizontale[k].velocities=[0]
+    voie_horizontale[k].velocities=[moving.Point(0,0)]
     voie_horizontale[k].geometry=shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
     voie_horizontale[k].userType=1
 
     for t in range(1,t_simul):
         p=moving.MovingObject.getPositions(voie_horizontale[k])[t-1].x
-        v=moving.MovingObject.getVelocities(voie_horizontale[k-1])[t]
-        velocite=v0
-        new_position=position(p,velocite,t)
+        v=moving.Point.norm2(moving.MovingObject.getVelocities(voie_horizontale[k-1])[t])
+        velocite=moving.Point(1,0).__mul__(moving.Point.norm2(v0))
+        new_position=position(p,moving.Point.norm2(velocite),t)
 
         s=gap(moving.MovingObject.getPositions(voie_horizontale[k-1])[t].x,new_position.x,L[k-1])
         smin=7
 
         if s<smin:
-            velocite=(v*t-L[k-1]-smin)/(t)
+            velocite=moving.Point(1,0).__mul__((v*t-L[k-1]-smin)/t)
 
-        if velocite<0:
-            velocite=0
-
+        if velocite.x<0:
+            velocite=moving.Point(0,0)
 
         voie_horizontale[k].velocities.append(velocite)
-        voie_horizontale[k].positions.append(position(p,velocite,1))
+        voie_horizontale[k].positions.append(position(p,moving.Point.norm2(velocite),1))
 
 create_yaml('voie_horizontale.yml',voie_horizontale)
-
 
 t=[]
 p=[]
 v=[]
 plt.figure()
 
-for k in range (0,5):
+for k in range (0,number_of_cars):
     p.append([])
+    v.append([])
     t.append(intervals[k])
-    v.append(voie_horizontale[k].velocities)
+
     for time in range(0,t_simul):
         p[k].append(voie_horizontale[k].positions[time].x)
+        v[k].append(moving.Point.norm2(voie_horizontale[k].velocities[time]))
 
-    plt.plot(t[k],v[k])
+
+
+    plt.plot(t[k],p[k])
     # plt.plot(t[k],p[k])
 # plt.plot(t,p)
 #
