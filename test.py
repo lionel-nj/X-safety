0ac6a2dc4adb0ac84b101e62c0426bf1cd56079c
Lionel NEBOT JANVIER <lionel@MacBook-Air-de-Lionel.local>
2018-09-11 21:03:14 -0400

    test de création d'interactions entre véhicules->calcul des indicateurs

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-import toolkit
-fron trafficintelligence import events
-fron trafficintelligence import moving
+from toolkit import *
+from trafficintelligence import events
+from trafficintelligence import moving
 
 #chargement des fichiers de trajectoires
 data_horizontal=load_yml('horizontal.yml')
-data_vertical=load_yml('vertical.yml)
+data_vertical=load_yml('vertical.yml')
 
 objet1=data_horizontal[0]
 objet2=data_vertical[0]
 objet3=data_horizontal[3]
 
-inter=events.createInteractions([objet1,objet2],objet3)
+inter=events.createInteractions([objet1,objet2],[objet3])
 #inter=liste d'objects de type Interactions
