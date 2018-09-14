875b14f6252bcf037e0e07c0d3b538b956f53b48
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-PO-35-16.nat.polymtl.ca>
2018-09-14 12:17:30 -0400

    -> classes

--- carsvsped.py
+++ carsvsped.py
@@ -9,10 +9,10 @@ traj_v=cars.voie.generate_trajectories(voie_verticale)[0]
 traj_h=cars.voie.generate_trajectories(voie_horizontale)[0]
 
 for k in traj_v:
-    traj_v[k].type_voie='verticale'
+    traj_v[k].etiquette='verticale'
 
 for k in traj_h:
-    traj_h[k].type_voie='horizontale'
+    traj_h[k].etiquette='horizontale'
 
 p=moving.Point(0,1000)
 n=10
@@ -23,56 +23,82 @@ ped.timeInterval=moving.Interval(30,40)
 ped.positions=moving.Trajectory.generate(p,v,n)[0]
 ped.velocities=[]
 ped.type_voie="test"
+ped.etiquette='horizontale'
 
-# traj_h[7]=ped
+pietons=dict()
+pietons[0]=ped
 
-def calcul_DY(obj1,obj2):
-    a=moving.MovingObject.getPositionAt(obj1,0).y
-    b=moving.MovingObject.getPositionAt(obj2,0).y
-    return a-b
+class essai():
 
-def calcul_DX(obj1,obj2):
-    a=moving.MovingObject.getPositionAt(obj1,0).x
-    b=moving.MovingObject.getPositionAt(obj2,0).x
-    return a-b
+    def __init__(self,voie1,voie2,ped):
+        self.voie1=voie1
+        self.voie2=voie2
+        self.ped=ped
 
-def modifier_vitesse(veh,t,new_speed):
-    veh.velocities[t]=new_speed
+    def calcul_DY(self,i,j,t):
+        a=moving.MovingObject.getPositionAt(self.voie1[i],t).y
+        b=moving.MovingObject.getPositionAt(self.voie2[j],t).y
+        return a-b
 
-def distance_min_verifiee_x(objet1_leader,objet2_follower,t):
-    if cars.gap(objet1_leader.positions[t].x,objet2_follower.positions[t].x,(objet1_leader.geometry.length-2*1.8)/2)>10:
-        return True
-    return False
+    def calcul_DX(self,i,j,t):
+        a=moving.MovingObject.getPositionAt(self.voie1[i],t).x
+        b=moving.MovingObject.getPositionAt(self.voie2[j],t).x
+        return a-b
 
-def distance_min_verifiee_y(objet1_leader,objet2_follower,t):
-    if cars.gap(objet1_leader.positions[t].y,objet2_follower.positions[t].y,(objet1_leader.geometry.length-2*1.8)/2)>10:
-        return True
-    return False
+    def distanceMinVerifiee(self,direction,voie,i,j,t,dmin):
+        if direction=='horizontale':
+            if voie==1:
+                if cars.gap(self.voie1[i].positions[t].x,self.voie1[j].positions[t].x,(self.voie1[i].geometry.length-2*1.8)/2)>dmin:
+                    return True
+            elif voie==2:
+                if cars.gap(self.voie2[i].positions[t].x,self.voie2[j].positions[t].x,(self.voie2[i].geometry.length-2*1.8)/2)>dmin:
+                    return True
+                return False
 
-def existing_users(voie1,voie2,t):
-    rep=[]
-    for k in range(0,len(voie1)):
-        if moving.Interval.contains(voie1[k].getTimeInterval(),t):
-            rep.append(voie1[k])
-    for k in range(0,len(voie2)):
-        if moving.Interval.contains(voie2[k].getTimeInterval(),t):
-            rep.append(voie2[k])
-    return rep
+        elif direction=='verticale':
+            if voie==2:
+                if cars.gap(self.voie2[i].positions[t].y,self.voie2[j].positions[t].y,(self.voie2[i].geometry.length-2*1.8)/2)>dmin:
+                    return True
+            elif voie==1:
+                if cars.gap(self.voie1[i].positions[t].y,self.voie1[j].positions[t].y,(self.voie1[i].geometry.length-2*1.8)/2)>dmin:
+                    return True
+                return False
 
-def type_of_user_ahead(objet,t,voie1,voie2):
-    dist=[]
-    utilisateurs_existants=existing_users(voie1,voie2,t)
-    for k in utilisateurs_existants:
-        if calcul_DY(objet,k)<0:
-            dist.append(float('inf'))
-        else:
-            dist.append(calcul_DY(objet,k))
-    return moving.MovingObject.getUserType(utilisateurs_existants[dist.index(min(dist))])
+    def existingUsers(self,t):
+        rep=[]
+        for k in range(0,len(self.voie1)):
+            if moving.Interval.contains(self.voie1[k].getTimeInterval(),t):
+                rep.append(self.voie1[k])
+        for k in range(0,len(self.voie2)):
+            if moving.Interval.contains(self.voie2[k].getTimeInterval(),t):
+                rep.append(self.voie2[k])
+        for k in range(0,len(self.ped)):
+            if moving.Interval.contains(self.ped[k].getTimeInterval(),t):
+                rep.append(self.ped[k])
+        return rep
+
+    def type_of_user_ahead(objet,t,voie1,voie2):
+        dist=[]
+        utilisateurs_existants=existingUsers(voie1,voie2,t)
+        if objet.etiquette=='verticale':
+            for k in utilisateurs_existants:
+                if calcul_DY(objet,k)<0:
+                    dist.append(float('inf'))
+                else:
+                    dist.append(calcul_DY(objet,k))
+            return moving.MovingObject.getUserType(utilisateurs_existants[dist.index(min(dist))])
+        elif objet.etiquette=='horizontale':
+            for k in utilisateurs_existants:
+                if calcul_DX(objet,k)<0:
+                    dist.append(float('inf'))
+                else:
+                    dist.append(calcul_DX(objet,k))
+            return moving.MovingObject.getUserType(utilisateurs_existants[dist.index(min(dist))])
 
 # t=30
 #
 # def calcul_vitesse_distance(leader,follower,t):
-#     if not distance_min_verifiee_y(leader,follower,t):
+#     if not distanceMinVerifiee(leader,follower,t):
 #         v=moving.Point.norm2(leader.velocities[t])
 #         L=leader.geometry.length-2*1.8)/2
 #         new_speed=(v*t-L-10)/t
