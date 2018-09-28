a05d9b860a17126b1c396f4083805ff08d304fd1
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-PO-41-202.nat.polymtl.ca>
2018-09-28 16:21:26 -0400

    curvilinear autres veh

--- cars.py
+++ cars.py
@@ -13,17 +13,17 @@ from math import sqrt
 # import position from toolkit
 # import gap from toolkit
 
-t_stop=30
-t_marche1=30
-t_marche2=30
-t_simul=t_marche1+t_marche2+t_stop
-number_of_cars=7
+t_stop = 30
+t_marche1 = 30
+t_marche2 = 30
+t_simul = t_marche1+t_marche2+t_stop
+number_of_cars = 7
 
 class flow():
 
     def __init__(self,direction,nom_fichier_sortie): #direction est un vecteur de la forme moving.Point(x,y)
-        self.direction=direction
-        self.nom_fichier_sortie=nom_fichier_sortie
+        self.direction = direction
+        self.nom_fichier_sortie = nom_fichier_sortie
 
 
     def gap(x_leader,x_following,L_leader):
@@ -31,9 +31,9 @@ class flow():
         return x_leader-x_following-L_leader
 
 
-    def positionV(y,v,t,p):
+    def positionV(preceding_position,speed,time,p):
         "fonctions de maj des positions"
-        return v*t+y
+        return speed*time+preceding_position
 
     # def positionH(y,v,t,p):
     #     "fonctions de maj des positions"
@@ -44,14 +44,14 @@ class flow():
 
         "définition des instants de création des véhicules"
 
-        tiv=generateSampleFromSample(number_of_cars)
-        h=list(itertools.accumulate(tiv))
+        tiv = generateSampleFromSample(number_of_cars)
+        h = list(itertools.accumulate(tiv))
 
-        delta_t=1
-        t_stop=30
-        t_marche1=30
-        t_marche2=30
-        t_simul=t_marche1+t_marche2+t_stop
+        delta_t = 1
+        t_stop = 30
+        t_marche1 = 30
+        t_marche2 = 30
+        t_simul = t_marche1+t_marche2+t_stop
 
         intervals=[None]*t_simul
 
@@ -65,113 +65,120 @@ class flow():
                 # Initialisation du premier véhicule
         ##########################################################
 
-        data_flow=dict()
+        data_flow = dict()
         data_flow[0]=moving.MovingObject()
 
-        l=random.normalvariate(6.5,0.3) #longueur du vehicule, random suivant une loi normale. a rectifier
+        l = random.normalvariate(6.5,0.3) #longueur du vehicule, random suivant une loi normale. a rectifier
         L=[]
         L.append(l)
-        # l=7
+        # l = 7
         S=[0]
+        Y=[2000]
 
-        if self.direction==moving.Point(0,1):
-            Y=[2000]
+        if self.direction == moving.Point(0,1):
             lanes=[1]
-            # list_of_curvilinear_positions=moving.Trajectory(positions=[[2000],[0]])
+            # list_of_curvilinear_positions = moving.Trajectory(positions=[[2000],[0]])
         else:
-            Y=[2000]
             lanes=[2]
 
-        list_of_curvilinear_positions=moving.CurvilinearTrajectory(S,Y,lanes)
-        # list_of_curvilinear_positions=moving.Trajectory(positions=[[0],[2000]])
-        v0=random.normalvariate(30,3.2)
-        # v0=self.direction.__mul__(random.normalvariate(30,3.2))
+        list_of_curvilinear_positions = moving.CurvilinearTrajectory(S,Y,lanes)
+        # list_of_curvilinear_positions = moving.Trajectory(positions=[[0],[2000]])
+        v0 = random.normalvariate(30,3.2)
+        # v0 = self.direction.__mul__(random.normalvariate(30,3.2))
         # speed=[moving.Point(0,0)]
-        speed=[v0]
+        speed = [v0]
 
         for t in range(0,t_simul):
             # speed.append(self.direction.__mul__(moving.Point.norm2(v0)))
             speed.append(v)
 
-        if self.direction==moving.Point(0,1):
+        if self.direction == moving.Point(0,1):
             for t in range(1,t_simul):
-                temp=flow.positionV(list_of_curvilinear_positions[t-1][0],v0,1,2000)
+                temp = flow.positionV(list_of_curvilinear_positions[t-1][0],v0,1,2000)
                 list_of_curvilinear_positions.addPositionSYL(temp,2000,1)
                 # list_of_curvilinear_positions.positions[0].append(temp.x)
                 # list_of_curvilinear_positions.positions[1].append(temp.y)
 
         else:
             for t in range(1,t_simul):
-                temp=flow.positionV(list_of_curvilinear_positions[t-1][0],v0,1,2000)
+                temp = flow.positionV(list_of_curvilinear_positions[t-1][0],v0,1,2000)
                 list_of_curvilinear_positions.addPositionSYL(temp,2000,2)
-                # temp=flow.positionH(list_of_curvilinear_positions[t-1].x,moving.Point.norm2(speed[t]),1,2000)
+                # temp = flow.positionH(list_of_curvilinear_positions[t-1].x,moving.Point.norm2(speed[t]),1,2000)
                 # list_of_curvilinear_positions.addPositionSYL(temp.x,temp.y,2)
 
                 # list_of_curvilinear_positions.positions[0].append(temp.x)
                 # list_of_curvilinear_positions.positions[1].append(temp.y)
 
         # data_flow[0].timeInterval=[0,300]
-        data_flow[0].timeInterval=moving.TimeInterval(0,300)
-        data_flow[0].curvilinearPositions=list_of_curvilinear_positions
-        data_flow[0].velocities=speed
-        data_flow[0].geometry=shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
-        data_flow[0].userType=1
+        data_flow[0].timeInterval = moving.TimeInterval(0,300)
+        data_flow[0].curvilinearPositions = list_of_curvilinear_positions
+        data_flow[0].velocities = speed
+        data_flow[0].geometry = shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
+        data_flow[0].userType = 1
 
 
         ##########################################################
                 # Initialisation des autres véhicules
         ##########################################################
 
+        lane=None
 
         for k in range(1,number_of_cars):
-            v0=self.direction.__mul__(random.normalvariate(30,3.2))
-            l=random.uniform(6,8)
+            v0 = random.normalvariate(30,3.2)
+            l = random.uniform(6,8)
             L.append(l)
 
-            data_flow[k]=moving.MovingObject()
+            data_flow[k] = moving.MovingObject()
             # data_flow[k].timeInterval=[intervals[k][0],300+intervals[k][0]]
-            data_flow[k].timeInterval=moving.TimeInterval(intervals[k][0],300+intervals[k][0])
+            data_flow[k].timeInterval = moving.TimeInterval(intervals[k][0],300+intervals[k][0])
 
-            if self.direction==moving.Point(0,1):
-                data_flow[k].positions=moving.Trajectory(positions=[[2000],[0]])
+            S = [0]
+            Y = [2000]
+
+            if self.direction == moving.Point(0,1):
+                lane = 1
             else:
-                data_flow[k].positions=moving.Trajectory(positions=[[0],[2000]])
+                lane = 2
 
-            data_flow[k].velocities=[moving.Point(0,0)]
-            data_flow[k].geometry=shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
-            data_flow[k].userType=1
+            lanes = [lane]
+
+            data_flow[k].curvilinearPositions = moving.curvilinearPositions(S,Y,lanes)
+
+            # if self.direction == moving.Point(0,1):
+            #     data_flow[k].curvilinearPositions = moving.Trajectory(positions=[[2000],[0]])
+            # else:
+            #     data_flow[k].curvilinearPositions = moving.Trajectory(positions=[[0],[2000]])
+
+            data_flow[k].velocities = [0]
+            data_flow[k].geometry = shapely.geometry.Polygon([(0,0),(0,1.8),(l,1.8),(l,0)])
+            data_flow[k].userType = 1
+
             for t in range(1,t_simul):
-                # p=moving.MovingObject.getPositions(data_flow[k])[t-1].y
-                if self.direction==moving.Point(0,1):
-                    p=data_flow[k].positions[t-1].y
-                else:
-                    p=data_flow[k].positions[t-1].x
+                # p = moving.MovingObject.getPositions(data_flow[k])[t-1].y
+                # if self.direction == moving.Point(0,1):
+                s = data_flow[k].curvilinearPositions[t-1][0]
+                # else:
+                    # p = data_flow[k].positions[t-1].x
+                v = moving.MovingObject.getVelocities(data_flow[k-1])[t]
+                velocite = v0
+                new_position = flow.positionV(s,velocite,t)
+                # s = gap(moving.MovingObject.getPositions(data_flow[k-1])[t].y,new_position.y,L[k-1])
+                # if self.direction == moving.Point(0,1):
+                s = flow.gap(data_flow[k-1].curvilinearPositions[t][0],new_position,L[k-1])
+                # else:
+                #     s = flow.gap(data_flow[k-1].positions[t].x,new_position.x,L[k-1])
 
-                v=moving.Point.norm2(moving.MovingObject.getVelocities(data_flow[k-1])[t])
-                velocite=self.direction.__mul__(moving.Point.norm2(v0))
-                new_position=flow.positionV(p,moving.Point.norm2(velocite),t,2000)
-                # s=gap(moving.MovingObject.getPositions(data_flow[k-1])[t].y,new_position.y,L[k-1])
-                if self.direction==moving.Point(0,1):
-                    s=flow.gap(data_flow[k-1].positions[t].y,new_position.y,L[k-1])
-                else:
-                    s=flow.gap(data_flow[k-1].positions[t].x,new_position.x,L[k-1])
-
-                smin=25
+                smin = 25 #a revoir .. distance de sécurité posée arbitrairement
 
                 if s<smin:
-                    velocite=self.direction.__mul__((v*t-L[k-1]-smin)/t)
+                    velocite = (v*t-L[k-1]-smin)/t
 
-                if self.direction==moving.Point(0,1):
-                    if velocite.y<0:
-                        velocite=moving.Point(0,0)
-                    data_flow[k].velocities.append(velocite)
-                    data_flow[k].positions.addPosition(flow.positionV(p,moving.Point.norm2(velocite),1,2000))
-                else:
-                    if velocite.x<0:
-                        velocite=moving.Point(0,0)
-                    data_flow[k].velocities.append(velocite)
-                    data_flow[k].positions.addPosition(flow.positionH(p,moving.Point.norm2(velocite),1,2000))
+                # if self.direction == moving.Point(0,1):
+                if velocite<0:
+                    velocite = 0
 
+                data_flow[k].velocities.append(velocite)
+                data_flow[k].curvilinearPositions.addPositionSYL(flow.positionV(p,moving.Point.norm2(velocite),1),2000,lane)
 
                 # data_flow[k].positions.append(position(p,moving.Point.norm2(velocite),1))
 
@@ -180,11 +187,11 @@ class flow():
         return data_flow, intervals
 
     def trace(self,data_flow):
-        t=[]
-        p=[]
-        v=[]
-        objet=data_flow.generateTrajectories()
-        ylabel=''
+        t = []
+        p = []
+        v = []
+        objet = data_flow.generateTrajectories()
+        ylabel = ''
 
         for k in range (0,number_of_cars):
             p.append([])
@@ -192,7 +199,7 @@ class flow():
             t.append(objet[1][k])
 
             for time in range(0,t_simul):
-                if self.direction==moving.Point(0,1):
+                if self.direction == moving.Point(0,1):
                     p[k].append(objet[0][k].positions[time].y)
                     ylabel="position selon l'axe x"
                 else:
