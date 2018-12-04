d071bb423721b584fcd4e34591a5b3900e49aa3c
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-LA-21-175.nat.polymtl.ca>
2018-12-04 16:58:57 -0500

    ajout des unites dans simulation.py

--- simulation.py
+++ simulation.py
@@ -1,6 +1,17 @@
 
 class Simulation(object):
     '''Stores all simulation and world parameters'''
+    units = [
+    'sec',
+    'sec',
+    'm',
+    'sec',
+    'm',
+    'm',
+    'm',
+    'm'
+    ]
+
     def __init__(self, duration, timeStep, interactionDistance, minimumTimeHeadway, averageVehicleLength, averageVehicleWidth, vehicleLengthSD, vehicleWidthSD):
         self.duration = duration
         self.timeStep = timeStep
