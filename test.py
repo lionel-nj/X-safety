737e9a2eb29d5b203da3fcff489a6d3819e24587
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-LA-21-175.nat.polymtl.ca>
2018-12-04 16:48:35 -0500

    definition d'un fichier defaut a chaque run de test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import trafficintelligence
+from trafficintelligence import moving
 import cars
 import objectsofworld
 import toolkit
@@ -6,6 +6,24 @@ import simulation
 import numpy as np
 
 world = objectsofworld.World.load('default.yml')
+
+align0 = objectsofworld.Alignment()
+align0.makeAlignment(entryPoint = moving.Point(-500,0),exitPoint = moving.Point(500,0))
+align0.idx = 0
+
+align1 = objectsofworld.Alignment()
+align1.makeAlignment( entryPoint = moving.Point(0,-500),exitPoint = moving.Point(0,500))
+align1.idx = 1
+
+alignments = [align0, align1]
+
+controlDevices = None
+
+vehicleInputs0 = cars.VehicleInput(alignmentIdx = align0.idx, fileName = "horizontal.yml", volume = 1000)
+vehicleInputs1 = cars.VehicleInput(alignmentIdx = align1.idx, fileName = "vertical.yml", volume = 1000)
+vehicleInputs = [vehicleInputs0, vehicleInputs1]
+
+world.makeDefault(alignments, controlDevices, vehicleInputs)
 
 sim = simulation.Simulation(72, 1., 30, 2, 7, 2, 1,.5) # second and meter
  # [cars.VehicleInput(0, 'horizontal.yml', 1000), cars.VehicleInput(1, 'vertical.yml', 500)]
