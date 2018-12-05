e9047a713f9c824e4adc9ed983848744f428295b
Lionel NEBOT JANVIER <lionel@service-WIFI-EDUROAM-PO-55-192.nat.polymtl.ca>
2018-12-05 15:15:21 -0500

    n- test

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ import numpy as np
 world = objectsofworld.World.load('default.yml')
 
 align0 = objectsofworld.Alignment()
-align0.makeAlignment(entryPoint = moving.Point(-500,0),exitPoint = moving.Point(500,0))
+align0.makeAlignment(entryPoint = moving.Point(-750,0),exitPoint = moving.Point(700,0))
 align0.idx = 0
 
 align1 = objectsofworld.Alignment()
-align1.makeAlignment( entryPoint = moving.Point(0,-500),exitPoint = moving.Point(0,500))
+align1.makeAlignment( entryPoint = moving.Point(0,-700),exitPoint = moving.Point(0,750))
 align1.idx = 1
 
 alignments = [align0, align1]
@@ -26,9 +26,8 @@ vehicleInputs = [vehicleInputs0, vehicleInputs1]
 world.makeDefault(alignments, controlDevices, vehicleInputs)
 
 sim = simulation.Simulation(72, 1., 30, 2, 7, 2, 1,.5) # second and meter
- # [cars.VehicleInput(0, 'horizontal.yml', 1000), cars.VehicleInput(1, 'vertical.yml', 500)]
+
 #creation des vehicules
-# world.vehicle
 vehicleInputs = world.vehicleInputs
 
 t_simul = sim.duration
@@ -38,8 +37,8 @@ averageVehicleLength = sim.averageVehicleLength
 vehicleLengthSD = sim.vehicleLengthSD
 vehicleWidthSD = sim.vehicleWidthSD
 
-volumes_to_test_on_0 = [k*100 for k in range(5, 16)]
-volumes_to_test_on_1 = [k*100 for k in range(5, 16)]
+# volumes_to_test_on_0 = [k*100 for k in range(5, 16)]
+# volumes_to_test_on_1 = [k*100 for k in range(5, 16)]
 
 vehiclesTrajectories = []
 for alignment, vehicleInput in zip(world.alignments, vehicleInputs):
@@ -51,7 +50,7 @@ toolkit.save_yaml('vertical.yml', vehiclesTrajectories[1])
 # #calcul du nombre d'interactions
 dmin = sim.interactionDistance
 #
-# affichage des matrices d'interactions
+# affichage des nombres/matrices d'interactions
 print(world.countAllEncounters(vehiclesTrajectories,dmin)[0])
 print(world.countAllEncounters(vehiclesTrajectories,dmin)[1])
 print(world.countAllEncounters(vehiclesTrajectories,dmin)[2])
