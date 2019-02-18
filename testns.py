a3929d9b04283239c0464a735b4ac7f4357d539c
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-02-18 16:50:00 -0500

    bug qui semble etre resolu avec Newell + utilisation d'objet TimeInterval

--- testns.py
+++ testns.py
@@ -29,7 +29,7 @@ for t in np.arange(0., sim.duration, sim.timeStep):
                                                                                               [vi.cumulatedHeadways[0],
                                                                                                sim.duration]))
                 vi.cumulatedHeadways.pop(0)
-        world.alignments[vi.alignmentIdx].vehicles = world.alignments[vi.alignmentIdx].vehicles[0:int(sim.duration*vi.volume/3600)]
+        world.alignments[vi.alignmentIdx].vehicles = world.alignments[vi.alignmentIdx].vehicles[0:round(sim.duration*vi.volume/3600)]
             # pass
 
         # todo: trouver les nouveaux vehicules apparaissant dans [t, t+timeStep[ et creer un MovingObject correspondant
