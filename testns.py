0289c12cbfae11e4ba0b25c7e22ecce7ff322488
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-02-26 09:24:51 -0500

    vehicleInputs to userInputs

--- testns.py
+++ testns.py
@@ -2,14 +2,16 @@ import objectsofworld
 import toolkit
 import itertools
 import random
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 world = objectsofworld.World.load('simple-net.yml')
 sim = toolkit.load_yaml('config.yml')
 
 random.seed(sim.seed)
 
-for vi in world.vehicleInputs:
+for vi in world.userInputs:
     # link to alignment
     for al in world.alignments:
         if al.idx == vi.alignmentIdx:
@@ -21,32 +23,22 @@ for vi in world.vehicleInputs:
 for al in world.alignments:
     al.vehicles = []
 
-for t in np.arange(0., sim.duration, sim.timeStep):
-    for vi in world.vehicleInputs:
-        for elt in vi.cumulatedHeadways:
-            if t <= vi.cumulatedHeadways[0] < t + sim.timeStep:
-                world.alignments[vi.alignmentIdx].vehicles.append(world.initVehicleOnAligment(vi.alignmentIdx,
-                                                                                              [vi.cumulatedHeadways[0],
-                                                                                               sim.duration]))
-                vi.cumulatedHeadways.pop(0)
-        world.alignments[vi.alignmentIdx].vehicles = world.alignments[vi.alignmentIdx].vehicles[0:round(sim.duration*vi.volume/3600)]
-            # pass
+userNum = 0
+for i in range(int(np.floor(sim.duration/sim.timeStep))):
+    print('simulation step {}'.format(i))
+    userNum = world.initUsers(i, sim.timeStep, userNum)
 
-        # todo: trouver les nouveaux vehicules apparaissant dans [t, t+timeStep[ et creer un MovingObject correspondant
-
-    # pass
     for al in world.alignments:
-        for idx, v in enumerate(al.vehicles):
+        for v in al.vehicles:
+            v.updateCurvilinearPositions("newell", i, sim.timeStep)
 
-            # sinon on met a jour les positions selon la valeur de t par rapport a celle du temps de reaction du conducteur
-            if idx == 0:
-                leaderVehicle = None
-            else:
-                leaderVehicle = al.vehicles[idx - 1]
+# display
+plt.figure()
+for al in world.alignments:
+    for v in al.vehicles:
+        if v.timeInterval is not None:
+            v.plotCurvilinearPositions()
+# plt.show()
+#TODO: avior les memes replications pour verifier le resultat de count selon diverses valeurs dmin
 
-            v.updateCurvilinearPositions(method="newell",
-                                         timeStep=sim.timeStep,
-                                         leaderVehicle=leaderVehicle,
-                                         nextAlignment_idx=0,
-                                         changeOfAlignment=False,
-                                         time=t)
+print(world.count('inLine', 25, 0))
