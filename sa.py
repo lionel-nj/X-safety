c7f9c2c1b5e2dfa7d83050376edcdaa1ddf6d0ff
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-06-04 17:20:27 -0400

    tests

--- sa.py
+++ sa.py
@@ -26,12 +26,12 @@ problem = dict(num_vars=4,
 paramValues = saltelli.sample(problem, args.nop, seed=0)
 
 _ttc = [0] * paramValues.shape[0]
-# _userCount = [0] * paramValues.shape[0]
-# _conflict5 = [0] * paramValues.shape[0]
-# _conflict10 = [0] * paramValues.shape[0]
-# _conflict15 = [0] * paramValues.shape[0]
-# _minDistance = [0] * paramValues.shape[0]
-# _meanDistance = [0] * paramValues.shape[0]
+_userCount = [0] * paramValues.shape[0]
+_conflict5 = [0] * paramValues.shape[0]
+_conflict10 = [0] * paramValues.shape[0]
+_conflict15 = [0] * paramValues.shape[0]
+_minDistance = [0] * paramValues.shape[0]
+_meanDistance = [0] * paramValues.shape[0]
 
 # _ttc = np.zeros([paramValues.shape[0]])
 for i, X in enumerate(paramValues):
@@ -46,15 +46,55 @@ for i, X in enumerate(paramValues):
     # world.userInputs[0].distributions['tau'].loc = X[4]
 
     ttc = []
+    userCount = []
+    conflict5 = []
+    conflict10 = []
+    conflict15 = []
+    minDistance = []
+    meanDistance = []
+
     for k in range(0, args.rep):
         simOutput = analysis.evaluateModel(world, sim, k)
         ttc.append(simOutput[0])
+        minDistance.append(simOutput[1])
+        meanDistance.append(simOutput[2])
+        userCount.append(simOutput[3])
+        conflict5.append(simOutput[4])
+        conflict10.append(simOutput[5])
+        conflict15.append(simOutput[6])
+
     _ttc[i] = ttc
+    _minDistance[i] = minDistance
+    _meanDistance[i] = meanDistance
+    _userCount[i] = userCount
+    _conflict5[i] = conflict5
+    _conflict10[i] = conflict10
+    _conflict15[i] = conflict15
 
 ttc = np.zeros([paramValues.shape[0]])
-for i, el in enumerate(_ttc):
-    ttc[i] = toolkit.notNoneMean(el)
+minDistance = np.zeros([paramValues.shape[0]])
+meanDistance = np.zeros([paramValues.shape[0]])
+userCount = np.zeros([paramValues.shape[0]])
+conflict5 = np.zeros([paramValues.shape[0]])
+conflict10 = np.zeros([paramValues.shape[0]])
+conflict15 = np.zeros([paramValues.shape[0]])
 
-toolkit.saveYaml('outputData/sensitivity-analysis/simulationOutput.yml', ttc)
+for i in range(paramValues.shape[0]):
+    ttc[i] = toolkit.notNoneMean(_ttc[i])
+    minDistance[i] = toolkit.notNoneMean(_minDistance[i])
+    meanDistance[i] = toolkit.notNoneMean(_meanDistance[i])
+    userCount[i] = toolkit.notNoneMean(_userCount[i])
+    conflict5[i] = toolkit.notNoneMean(_conflict5[i])
+    conflict10[i] = toolkit.notNoneMean(_conflict10[i])
+    conflict15[i] = toolkit.notNoneMean(_conflict15[i])
+
+toolkit.saveYaml('outputData/sensitivity-analysis/ttc.yml', ttc)
+toolkit.saveYaml('outputData/sensitivity-analysis/mindistance.yml', minDistance)
+toolkit.saveYaml('outputData/sensitivity-analysis/meandistance.yml', meanDistance)
+toolkit.saveYaml('outputData/sensitivity-analysis/usercount.yml', userCount)
+toolkit.saveYaml('outputData/sensitivity-analysis/conflict5.yml', conflict5)
+toolkit.saveYaml('outputData/sensitivity-analysis/conflict10.yml', conflict10)
+toolkit.saveYaml('outputData/sensitivity-analysis/conflict15.yml', conflict15)
+
 # Si = sobol.analyze(problem, _ttc, print_to_console=False)
 toolkit.callWhenDone()
