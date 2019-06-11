861bc9d819b816026ec1810360b727308f675fc3
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-06-11 18:20:22 -0400

    fichiers de test

--- eval1rep.py
+++ eval1rep.py
@@ -5,27 +5,44 @@ import pandas
 import analysis
 import network
 import simulation
+import toolkit
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--headway", type=float, help="mean headway")
 parser.add_argument("--speed", type=float, help="mean desired velocity")
 parser.add_argument("--dn", type=float, help="mean delta")
 parser.add_argument("--tau", type=float, help="mean tau")
 parser.add_argument("--l", type=float, help="mean vehicle length")
 parser.add_argument("--duration", type=int, help="duration")
 parser.add_argument("--seed", type=int, help="seed")
-
 args = parser.parse_args()
-sim = simulation.Simulation.load('config.yml')
-sim.duration = args.duration
-world = network.World.load('simple-net.yml')
-world.assignValue(args)
-simOutput = analysis.evaluateModel2(world, sim, args.seed, '1eval')
 
-ttc = simOutput[0]
-pet = simOutput[-1]
+headways = [1.5, 1.8, 2.1, 2.4]
+seeds = [k for k in range(0, 25)]
 
-data = pandas.DataFrame(data=[ttc, pet],
-                        index=['ttc', 'pet'])
+for h in headways:
 
-data.to_csv('outputData/evaluation1rep-h={}-seed={}.csv'.format(args.headway, args.seed))
+    for seed in seeds:
+
+        sim = simulation.Simulation.load('config.yml')
+        sim.duration = args.duration
+        sim.seed = seed
+        world = network.World.load('simple-net.yml')
+
+        world.userInputs[0].distributions['headway'].scale = h - 1
+        world.userInputs[0].distributions['speed'].loc = args.speed
+        world.userInputs[0].distributions['dn'].loc = args.dn
+        world.userInputs[0].distributions['tau'].loc = args.tau
+        world.userInputs[0].distributions['length'].loc = args.l
+
+        simOutput = analysis.evaluateModel2(world, sim, seed, '1eval')
+
+        ttc = simOutput[0]
+        pet = simOutput[-1]
+
+        data = pandas.DataFrame(data=[ttc, pet],
+                                index=['ttc', 'pet'])
+
+        data.to_csv('outputData/evaluation1rep-h={}-seed={}.csv'.format(h, seed))
+
+
+toolkit.callWhenDone()
