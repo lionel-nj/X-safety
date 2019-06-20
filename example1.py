ae0f422f9dfe7d05fb233522c25f3404061f0b4f
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-06-19 21:48:35 -0400

    suite aux commentaires de N.S

---
+++ example1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+import network
+import simulation
+
+world = network.World.load('simple-net.yml')
+sim = simulation.Simulation.load('config.yml')   
+sim.run(world)
+world.plotUserTrajectories(sim.timeStep)
