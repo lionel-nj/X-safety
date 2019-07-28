0b6e5237cb0310c92b111d10c302070a087d3854
Lionel NEBOT JANVIER <lionel.nebot-janvier@polymtl.ca>
2019-07-28 13:57:14 -0400

    pedestrians

---
+++ graphiques_cd.py
@@ -0,0 +1,53 @@
+stop_nInter10 = {1: 5.133333333333334, 2: 364.2}
+
+stop_nInter20 = {1: 39.666666666666664, 2: 562.2}
+
+stop_nInter50 = {1: 199.86666666666667, 2: 1135.8}
+
+yield_nInter50 = {1: 199.8, 2: 1098.0}
+
+yield_nInter20 = {1: 42.86666666666667, 2: 532.3333333333334}
+
+yield_nInter10 = {1: 9.133333333333333, 2: 326.93333333333334}
+
+sQuo_nInter10 = {1: 0.0, 2: 145.33333333333334}
+
+sQuo_nInter20 = {1: 52.6, 2: 287.1333333333333}
+
+sQuo_nInter50 = {1: 315.3333333333333, 2: 723.2666666666667}
+
+import matplotlib.pyplot as plt
+
+x = ['10', '20', '50']
+y_stop_1 = [5, 39, 199]
+y_stop_2 = [364, 562, 1135]
+
+y_yield_1 = [9, 42, 199]
+y_yield_2 = [326, 532, 1098]
+
+y_sQuo_1 = [0, 52, 315]
+y_sQuo_2 = [145, 287, 723]
+
+plt.plot(x, y_stop_1)
+plt.plot(x, y_yield_1)
+plt.plot(x, y_sQuo_1)
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.xlabel('interaction distance (m)')
+plt.ylabel('nInter')
+plt.savefig('rearEnd-nInter_variations.pdf')
+
+plt.close()
+
+plt.plot(x, y_stop_2)
+plt.plot(x, y_yield_2)
+plt.plot(x, y_sQuo_2)
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.xlabel('interaction distance (m)')
+plt.ylabel('nInter')
+plt.savefig('side-nInter_variations.pdf')
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+
+plt.close()
--- statistics.py
+++ statistics.py
@@ -1,3 +1,5 @@
+import collections
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
@@ -62,40 +64,178 @@ f.close()
 ### histograms ###
 
 # rear end TTCs
-plt.hist(stop_minTTCs[1], density=True, histtype='step', cumulative=True, label='stop sign', fill=False)
-plt.hist(yield_minTTCs[1], density=True, histtype='step', cumulative=True, label='yield sign', fill=False)
-plt.hist(sQuo_minTTCs[1], density=True, histtype='step', cumulative=True, label='status quo', fill=False)
+rearEnd_stop_minTTCs_x = np.sort(stop_minTTCs[1])
+rearEnd_yield_minTTCs_x = np.sort(yield_minTTCs[1])
+rearEnd_sQuo_minTTCs_x = np.sort(sQuo_minTTCs[1])
 
-plt.legend(loc='right')
+## side_stop_minTTCs_x = np.sort(stop_minTTCs[2])
+## side_yield_minTTCs_x = np.sort(yield_minTTCs[2])
+## side_sQuo_minTTCs_x = np.sort(sQuo_minTTCs[2])
+
+
+normedRearEnd_stop_minTTCs_y = 1. * np.arange(len(stop_minTTCs[1])) / (len(stop_minTTCs[1]) - 1)
+rawRearEnd_stop_minTTCs_y = 1. * np.arange(len(stop_minTTCs[1]))
+
+normedRearEnd_yield_minTTCs_y = 1. * np.arange(len(yield_minTTCs[1])) / (len(yield_minTTCs[1]) - 1)
+rawRearEnd_yield_minTTCs_y = 1. * np.arange(len(yield_minTTCs[1]))
+
+normedRearEnd_sQuo_minTTCs_y = 1. * np.arange(len(sQuo_minTTCs[1])) / (len(sQuo_minTTCs[1]) - 1)
+rawRearEnd_sQuo_minTTCs_y = 1. * np.arange(len(sQuo_minTTCs[1]))
+
+plt.plot(rearEnd_stop_minTTCs_x, normedRearEnd_stop_minTTCs_y)
+plt.plot(rearEnd_yield_minTTCs_x, normedRearEnd_yield_minTTCs_y)
+plt.plot(rearEnd_sQuo_minTTCs_x, normedRearEnd_sQuo_minTTCs_y)
+
+plt.xlabel('Time to Collision (s)')
+plt.ylabel('Frequency')
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
 plt.title('Rear end Time to Collision cumulative distribution functions')
+plt.savefig('cdfNormed-minTTCs-rearEnd.pdf')
+plt.close()
+
+plt.plot(rearEnd_stop_minTTCs_x, rawRearEnd_stop_minTTCs_y)
+plt.plot(rearEnd_yield_minTTCs_x, rawRearEnd_yield_minTTCs_y)
+plt.plot(rearEnd_sQuo_minTTCs_x, rawRearEnd_sQuo_minTTCs_y)
+
+plt.xlabel('Time to Collision (s)')
+plt.ylabel('Number of observations')
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Rear end Time to Collision cumulative distribution functions')
+plt.savefig('cdfRaw-minTTCs-rearEnd.pdf')
+plt.close()
+###############################################################
+
+# side TTCs
+
+side_stop_minTTCs_x = np.sort(stop_minTTCs[2])
+side_yield_minTTCs_x = np.sort(yield_minTTCs[2])
+side_sQuo_minTTCs_x = np.sort(sQuo_minTTCs[2])
+
+
+normedSide_stop_minTTCs_y = 1. * np.arange(len(stop_minTTCs[2])) / (len(stop_minTTCs[2]) - 1)
+rawSide_stop_minTTCs_y = 1. * np.arange(len(stop_minTTCs[2]))
+
+normedSide_yield_minTTCs_y = 1. * np.arange(len(yield_minTTCs[2])) / (len(yield_minTTCs[2]) - 1)
+rawSide_yield_minTTCs_y = 1. * np.arange(len(yield_minTTCs[2]))
+
+normedSide_sQuo_minTTCs_y = 1. * np.arange(len(sQuo_minTTCs[2])) / (len(sQuo_minTTCs[2]) - 1)
+rawSide_sQuo_minTTCs_y = 1. * np.arange(len(sQuo_minTTCs[2]))
+
+plt.plot(side_stop_minTTCs_x, normedSide_stop_minTTCs_y)
+plt.plot(side_yield_minTTCs_x, normedSide_yield_minTTCs_y)
+plt.plot(side_sQuo_minTTCs_x, normedSide_sQuo_minTTCs_y)
+
 plt.xlabel('Time to Collision (s)')
 plt.ylabel('Frequency')
 
-plt.savefig('cdf-rearEndTTCs.pdf')
-plt.close()
-
-#side TTCs
-plt.hist(stop_minTTCs[2], density=True, histtype='step', cumulative=True, label='stop sign', fill=False)
-plt.hist(yield_minTTCs[2], density=True, histtype='step', cumulative=True, label='yield sign', fill=False)
-plt.hist(sQuo_minTTCs[2], density=True, histtype='step', cumulative=True, label='status quo', fill=False)
-
-plt.legend(loc='right')
+plt.legend(['stop sign', 'yield sign', 'status quo'])
 plt.title('Side Time to Collision cumulative distribution functions')
+plt.savefig('cdfNormed-minTTCs-side.pdf')
+plt.close()
+
+plt.plot(side_stop_minTTCs_x, rawSide_stop_minTTCs_y)
+plt.plot(side_yield_minTTCs_x, rawSide_yield_minTTCs_y)
+plt.plot(side_sQuo_minTTCs_x, rawSide_sQuo_minTTCs_y)
+
+plt.xlabel('Time to Collision (s)')
+plt.ylabel('Number of observations')
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Rear end Time to Collision cumulative distribution functions')
+plt.savefig('cdfRaw-minTTCs-side.pdf')
+plt.close()
+
+############################################################
+# PETs
+
+stop_PETs_x = np.sort(stop_PETs)
+yield_PETs_x = np.sort(yield_PETs)
+sQuo_PETs_x = np.sort(sQuo_PETs)
+
+normedStop_PETs_y = 1. * np.arange(len(stop_PETs)) / (len(stop_PETs) - 1)
+rawStop_PETs_y = 1. * np.arange(len(stop_PETs))
+
+normedYield_PETs_y = 1. * np.arange(len(yield_PETs)) / (len(yield_PETs) - 1)
+rawYield_PETs_y = 1. * np.arange(len(yield_PETs))
+
+normedSquo_PETs_y = 1. * np.arange(len(sQuo_PETs)) / (len(sQuo_PETs) - 1)
+rawSquo_PETs_y = 1. * np.arange(len(sQuo_PETs))
+
+plt.plot(stop_PETs_x, normedStop_PETs_y)
+plt.plot(yield_PETs_x, normedYield_PETs_y)
+plt.plot(sQuo_PETs_x, normedSquo_PETs_y)
+
 plt.xlabel('Time to Collision (s)')
 plt.ylabel('Frequency')
 
-plt.savefig('cdf-sideTTCs.pdf')
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Post Encroachment Time cumulative distribution functions')
+plt.savefig('cdfNormed-PETs.pdf')
 plt.close()
 
-# PETs
-plt.hist(stop_PETs, density=True, histtype='step', cumulative=True, label='stop sign', fill=False)
-plt.hist(yield_PETs, density=True, histtype='step', cumulative=True, label='yield sign', fill=False)
-plt.hist(sQuo_PETs, density=True, histtype='step', cumulative=True, label='status quo', fill=False)
+plt.plot(stop_PETs_x, rawStop_PETs_y)
+plt.plot(yield_PETs_x, rawYield_PETs_y)
+plt.plot(sQuo_PETs_x, rawSquo_PETs_y)
 
-plt.legend(loc='right')
-plt.title('Post Encroachment Time Cumulative Distribution Functions')
-plt.xlabel('Post Encroachment Time (s)')
+plt.xlabel('Time to Collision (s)')
+plt.ylabel('Number of observations')
+
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Post Encroachment Time cumulative distribution functions')
+plt.savefig('cdfRaw-PETs.pdf')
+plt.close()
+
+
+# min distance distributions
+
+yield_minDistancesRearEnd = []
+yield_minDistancesSide = []
+
+stop_minDistancesRearEnd = []
+stop_minDistancesSide = []
+
+sQuo_minDistancesRearEnd = []
+sQuo_minDistancesSide = []
+
+for seed in seeds:
+    yield_minDistancesRearEnd.extend(yield_minDistances[1][seed])
+    yield_minDistancesSide.extend(yield_minDistances[2][seed])
+
+    stop_minDistancesRearEnd.extend(stop_minDistances[1][seed])
+    stop_minDistancesSide.extend(stop_minDistances[2][seed])
+
+    sQuo_minDistancesRearEnd.extend(sQuo_minDistances[1][seed])
+    sQuo_minDistancesSide.extend(sQuo_minDistances[2][seed])
+
+
+yield_minDistancesRearEndCounter = collections.Counter(yield_minDistancesRearEnd)
+yield_minDistancesSideCounter = collections.Counter(yield_minDistancesSide)
+
+stop_minDistancesRearEndCounter = collections.Counter(stop_minDistancesRearEnd)
+stop_minDistancesSideCounter = collections.Counter(stop_minDistancesSide)
+
+sQuo_minDistancesRearEndCounter = collections.Counter(sQuo_minDistancesRearEnd)
+sQuo_minDistancesSideCounter = collections.Counter(sQuo_minDistancesSide)
+
+plt.plot(stop_minDistancesRearEndCounter.keys(), stop_minDistancesRearEndCounter.values())
+plt.plot(yield_minDistancesRearEndCounter.keys(), yield_minDistancesRearEndCounter.values())
+plt.plot(sQuo_minDistancesRearEndCounter.keys(), sQuo_minDistancesRearEndCounter.values())
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Rear-end minimum distance cumulative distribution functions')
+plt.xlabel('Distances (m)')
 plt.ylabel('Frequency')
-
-plt.savefig('cdf-PETs.pdf')
+plt.savefig('cdf-rearEndminDistances.pdf')
 plt.close()
+
+plt.plot(stop_minDistancesSideCounter.keys(), stop_minDistancesSideCounter.values())
+plt.plot(yield_minDistancesSideCounter.keys(), yield_minDistancesSideCounter.values())
+plt.plot(sQuo_minDistancesSideCounter.keys(), sQuo_minDistancesSideCounter.values())
+plt.legend(['stop sign', 'yield sign', 'status quo'])
+plt.title('Rear-end minimum distance cumulative distribution functions')
+plt.xlabel('Distances (m)')
+plt.ylabel('Frequency')
+plt.savefig('cdf-sideMinDistances.pdf')
+plt.close()
+
